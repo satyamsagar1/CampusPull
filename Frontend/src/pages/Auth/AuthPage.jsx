11c8a64204a848bf155bb236618c3c1324f30755
Satyam Sagar <115887992+satyamsagar1@users.noreply.github.com>
2025-12-22 11:41:23 +0530

    Update AuthPage.jsx

--- Frontend/src/pages/Auth/AuthPage.jsx
+++ Frontend/src/pages/Auth/AuthPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useAuth } from "../../context/authContext";
+import { useAuth } from "../../context/AuthContext";
 import {
   FaUser, FaEnvelope, FaLock, FaUniversity, FaCalendarAlt,
   FaPhone, FaLinkedin, FaInfoCircle, FaTools, FaBuilding,
